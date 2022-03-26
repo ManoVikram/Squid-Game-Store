c8b51bcd1d10dc9890da9b20e397017e5839104b
Mano Vikram <manovik18@gmail.com>
2022-03-27 02:04:09 +0530

    The Home Screen UI is almost fine! Some basic animations needs to be added!

--- lib/screens/homeScreen/home_screen.dart
+++ lib/screens/homeScreen/home_screen.dart
@@ -19,7 +19,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       // backgroundColor: Color.fromARGB(255, 248, 247, 247),
       backgroundColor: const Color(0xFFF8F8F8),
-      bottomNavigationBar: BottomNavigationBar(
+      /* bottomNavigationBar: BottomNavigationBar(
         backgroundColor: const Color(0xFFF8F8F8),
         elevation: 0.0,
         items: [
@@ -80,6 +80,74 @@ class HomeScreen extends StatelessWidget {
               "lib/screens/homeScreen/assets/icons/Setting.svg",
               height: 26.0,
             ),
+          ),
+        ],
+      ), */
+      bottomNavigationBar: Stack(
+        children: [
+          Container(
+            height: _size.height * 0.16,
+            width: double.infinity,
+            color: const Color(0xFFF8F8F8),
+            padding: const EdgeInsets.all(defaultPadding / 2),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        height: 50.0,
+                        width: 50.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(
+                            colors: [
+                              primaryColor.withOpacity(0.4),
+                              primaryColor,
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              offset: const Offset(0.0, 10.0),
+                              color: primaryColor.withOpacity(0.32),
+                              blurRadius: 24.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SvgPicture.asset(
+                        "lib/screens/homeScreen/assets/icons/Home.svg",
+                        height: 24.0,
+                      ),
+                    ],
+                  ),
+                  SvgPicture.asset(
+                    "lib/screens/homeScreen/assets/icons/Buy.svg",
+                    height: 24.0,
+                  ),
+                  SvgPicture.asset(
+                    "lib/screens/homeScreen/assets/icons/Heart.svg",
+                    color: const Color(0xFF8B8BA1),
+                    height: 24.0,
+                  ),
+                  SvgPicture.asset(
+                    "lib/screens/homeScreen/assets/icons/Setting.svg",
+                    height: 24.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 48.0,
+            left: _size.width / 4 - 45.0,
+            child: SvgPicture.asset(
+                "lib/screens/homeScreen/assets/icons/SelectedNavButtonBottomElement.svg"),
           ),
         ],
       ),
